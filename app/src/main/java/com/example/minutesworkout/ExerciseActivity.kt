5e628ed16e8075b2139d31cb2b64bc28bb354449
MuhammadSaad420 <saad.oraxtech@gmail.com>
2023-04-12 12:10:16 +0500

    Using coundowntimers to display the senconds remaining

--- app/src/main/java/com/example/minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/minutesworkout/ExerciseActivity.kt
@@ -2,10 +2,14 @@ package com.example.minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     var binding: ActivityExerciseBinding? = null;
+    var restTimer: CountDownTimer? = null;
+    var restProgress: Int = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -18,5 +22,39 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbar?.setNavigationOnClickListener {
             onBackPressed()
         }
+        setUpTimer();
+    }
+
+    private fun setUpTimer() {
+        if(restTimer != null) {
+            restTimer = null;
+            restProgress = 0
+        }
+        startTimer()
+    }
+
+    private fun startTimer() {
+        restTimer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++;
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Here now we will start the exercise.", Toast.LENGTH_SHORT).show()
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null;
+        if(restTimer != null) {
+            restTimer = null;
+            restProgress = 0
+        }
+
     }
 }
